72ce622811fb274ce4d46cc51e537406a80225ea
Robby Rice <55925561+Ricerob@users.noreply.github.com>
2020-12-07 20:27:55 -0600

    Upload script, css changes
    Added github icon through iconmonstr with hover and link to my github profile. Small css changes. Added comments to script.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var backgroundColor = "#";
+var backgroundColor = "#";                                                      //Initializes the background css to #
 
+/*
+ * Function that returns a random color, represented by a 6 digit hex value
+ * RETURN: String "#xxxxxx", where x is 
+ */
 function randomize() {
-    backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-    if(backgroundColor.length===7){
-        return backgroundColor;
+    backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);    //Adds 6 numbers, converted to base 16 hexadecimal
+    if(backgroundColor.length === 7){                                           //If length is the same value and type as int 7...
+        return backgroundColor;                                                     //Return background color successfully
     }
-    else{
-        randomize()
+    else{                                                                       //Else...
+        randomize()                                                                 //Call the function again
     }
 }
 
-document.getElementById("main").style.backgroundColor = randomize();
-document.getElementById("color_displayer").innerHTML = backgroundColor;
+document.getElementById("main").style.backgroundColor = randomize();            //Sets main element's style to the result of the randomize() function
+document.getElementById("color_displayer").innerHTML = backgroundColor;         //Sets color_displayer to display the background color's value
